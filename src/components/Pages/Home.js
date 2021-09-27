68535294a933fbf637da54862d8cc53267e35f01
Sam007k <msameer007k@gmail.com>
2021-09-27 20:06:12 +0530

    first commit

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,13 @@
+import Search from "../../components/Users/Search";
+import Users from "../../components/Users/Users";
+
+const Home = () => {
+  return (
+    <>
+      <Search />
+      <Users />
+    </>
+  );
+};
+
+export default Home;
